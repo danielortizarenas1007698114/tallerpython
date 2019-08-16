c4631d0bd50146cd0f4153b80f3b6264fcfb58e5
danielortizarenas1007698114 <51802522+danielortizarenas1007698114@users.noreply.github.com>
2019-08-16 10:31:55 -0500

    1

---
+++ 1.py
@@ -0,0 +1,12 @@
+
+n1 = int(input("ingrese el numero: "))
+n2 = int(input("ingrese el segundo numero: "))
+
+producto = n1 * n2
+def punto1():
+    if producto > 1000:
+        return producto
+
+print(punto1())
+
+print("la suma de los valores ingresados es: ",(n1+n2))
