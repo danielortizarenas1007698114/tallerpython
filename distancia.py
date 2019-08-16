eddb41dca6c40037f68ebd5fd87bf2970e36203b
danielortizarenas1007698114 <51802522+danielortizarenas1007698114@users.noreply.github.com>
2019-08-16 10:37:24 -0500

    9

---
+++ distancia.py
@@ -0,0 +1,10 @@
+import math
+
+x2 = float(int("ingrese x2:"))
+x1 = float(int("ingrese x1:"))
+y2 = float(int("ingrese y2:"))
+y1 = float(int("ingrese y1:"))
+
+d = math.sqrt(math.pow((x2-x1),2)+math.pow((y2-y1),2))
+
+print("distancia: ",d)
